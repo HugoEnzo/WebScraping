b6f968782fd8b735f002ac895f199520c19e63a5
HugoEnzo <sivasankaranirudh@google.com>
2023-02-24 00:18:11 -0500

    @	modified:   Scrapy_Tutorials/Scrapy_Tutorials/pipelines.py @	modified:   Scrapy_Tutorials/Scrapy_Tutorials/settings.py

--- Scrapy_Tutorials/Scrapy_Tutorials/pipelines.py
+++ Scrapy_Tutorials/Scrapy_Tutorials/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
+import pymongo
 
 
-class ScrapyTutorialsPipeline:
+class MongodbPipeline:
+    collection_name = 'transcripts'
+    def open_spider(self, spider):
+        logging.warning('Spider opened')
+        self.client = pymongo.MongoClient("") #assigning and instance of MongoDB client
+        self.db = self.client['Database_01214']#creating a database
+
+
+    def close_spider(self, spider):
+        logging.warning('Spider Closed')
+        self.client.close()#closing the client after the execution is over
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
